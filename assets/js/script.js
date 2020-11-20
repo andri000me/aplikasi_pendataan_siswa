904951d4a5fe3aed9e58e56a8ea7d9abfb848123
candradwicahyo <52719874+candradwicahyo@users.noreply.github.com>
2020-11-21 01:23:25 +0700

    membuat file script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+  | nama : candra dwi cahyo
+  | umur : 16 tahun
+  | email : candradwicahyo18@gmail.com
+*/
+
+
+$(document).ready(function() {
+  
+  // menggunakan plugin datatables()
+  $('#myTable').dataTable();
+  
+});
